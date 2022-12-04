582dec7d5538a552f975ef982df64e6407b667d9
Cole Nussear <80608048+Nusse0712@users.noreply.github.com>
2022-12-04 12:15:26 -0500

    Add files via upload

--- lightshowpython.ino
+++ lightshowpython.ino
@@ -6,42 +6,99 @@ FASTLED_USING_NAMESPACE
 #define PART_PIN     12
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
-#define NUM_LEDS    150
+#define NUM_LEDS    120
 #define FRAMES_PER_SECOND  120
-#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 
-int RestfadeAmount = 3; 
-int Restbrightness = 0; 
 
-int HypfadeAmount = 8; 
-int Hypbrightness = 0; 
 
 CRGB leds[NUM_LEDS];
 CRGB partLeds[NUM_LEDS];
 uint8_t paletteIndex = 0;
 
-typedef void (*SimplePatternList[])();
 
-uint8_t gCurrentPatternNumber = 0;
 
 
 char command;
 boolean newData = false;
+CRGBPalette16 restingGreenPal = CRGBPalette16 (
+                               CRGB::Black,
+                               CRGB::Green,
+                               CRGB::Black,
+                               CRGB::Green,
 
+                               CRGB::Black,
+                               CRGB::Green,
+                               CRGB::Green,
+                               CRGB::Black,
+
+                               CRGB::Green,
+                               CRGB::Green,
+                               CRGB::Green,
+                               CRGB::Black,
+
+                               CRGB::Green,
+                               CRGB::Green,
+                               CRGB::Green,
+                               CRGB::Green
+                             );
 CRGBPalette16 hypGreenPal = CRGBPalette16 (
                                CRGB::Black,
+                               CRGB::DarkViolet,
                                CRGB::Black,
-                               CRGB::Black,
-                               CRGB::Black,
+                               CRGB::Green,
 
                                CRGB::Black,
-                               CRGB::Black,
-                               CRGB::Black,
+                               CRGB::Green,
+                               CRGB::Green,
+                               CRGB::DarkViolet,
+
+                               CRGB::Green,
+                               CRGB::Green,
+                               CRGB::Green,
                                CRGB::Black,
 
+                               CRGB::Green,
+                               CRGB::Green,
+                               CRGB::Green,
+                               CRGB::Green
+                             );
+                         
+                             
+CRGBPalette16 restingPurplePal = CRGBPalette16 (
+                               CRGB::Black,
+                               CRGB::Green,
+                               CRGB::Green,
+                               CRGB::DarkViolet,
+
+                               CRGB::Green,
+                               CRGB::DarkViolet,
+                               CRGB::DarkViolet,
+                               CRGB::Green,
+
+                               CRGB::DarkViolet,
+                               CRGB::DarkViolet,
+                               CRGB::Green,
+                               CRGB::Black,
+
+                               CRGB::Green,
+                               CRGB::DarkViolet,
+                               CRGB::Green,
+                               CRGB::DarkViolet
+                             );
+CRGBPalette16 earlyDepoGreen = CRGBPalette16 (
                                CRGB::Black,
                                CRGB::Black,
+                               CRGB::Green,
+                               CRGB::Green,
+
                                CRGB::Black,
+                               CRGB::Green,
+                               CRGB::Green,
+                               CRGB::Black,
+
+                               CRGB::Green,
+                               CRGB::Black,
+                               CRGB::Green,
                                CRGB::Black,
 
                                CRGB::Green,
@@ -74,22 +131,22 @@ CRGBPalette16 repolPurplePal = CRGBPalette16(
 CRGBPalette16 repolGreenPal = CRGBPalette16 (
                                 CRGB::Black,
                                 CRGB::Black,
-                                CRGB::Black,
+                                CRGB::DarkViolet,
                                 CRGB::Black,
 
+                                CRGB::DarkViolet,
                                 CRGB::Green,
-                                CRGB::Green,
-                                CRGB::Green,
-                                CRGB::Green,
+                                CRGB::DarkViolet,
+                                CRGB::DarkViolet,
 
                                 CRGB::Green,
-                                CRGB::Green,
+                                CRGB::DarkViolet,
                                 CRGB::Green,
                                 CRGB::DarkViolet,
 
                                 CRGB::Black,
-                                CRGB::Black,
-                                CRGB::Black,
+                                CRGB::DarkViolet,
+                                CRGB::DarkViolet,
                                 CRGB::DarkViolet
                              );
 CRGBPalette16 depoPurpPal = CRGBPalette16(
@@ -108,134 +165,14 @@ CRGBPalette16 depoPurpPal = CRGBPalette16(
                                CRGB::DarkViolet,
                                CRGB::DarkViolet,
 
-                               CRGB::Green,
-                               CRGB::Green,
+                               CRGB::DarkViolet,
+                               CRGB::DarkViolet,
                                CRGB::DarkViolet,
                                CRGB::DarkViolet
                                 
                               );
                           
-CRGBPalette16 restingGreenPal = CRGBPalette16 (
-                               CRGB::Black,
-                               CRGB::Black,
-                               CRGB::Black,
-                               CRGB::Black,
 
-                               CRGB::Black,
-                               CRGB::Black,
-                               CRGB::Black,
-                               CRGB::Black,
-
-                               CRGB::Black,
-                               CRGB::Black,
-                               CRGB::Black,
-                               CRGB::Black,
-
-                               CRGB::Green,
-                               CRGB::Black,
-                               CRGB::DarkViolet,
-                               CRGB::DarkViolet
-                             );
-                             
-CRGBPalette16 restingPurpPal = CRGBPalette16(
-                                CRGB::DarkViolet,
-                                CRGB::DarkViolet,
-                                CRGB::Green,
-                                CRGB::Green,
-
-                                CRGB::Green,
-                                CRGB::Green,
-                                CRGB::Green,
-                                CRGB::Green,
-
-                                CRGB::Green,
-                                CRGB::Green,
-                                CRGB::Green,
-                                CRGB::Green,
-
-                                CRGB::Green,
-                                CRGB::Green,
-                                CRGB::DarkViolet,
-                                CRGB::DarkViolet
-                                
-                              );
-
-
-
-CRGBPalette16 earlyDepoPal = CRGBPalette16 (
-        CRGB::White, 
-        CRGB::White, 
-        CRGB::Goldenrod,
-        CRGB::Goldenrod, 
-
-        CRGB::Goldenrod, 
-        CRGB::Orange, 
-        CRGB::Orange, 
-        CRGB::Goldenrod, 
-
-        CRGB::Goldenrod,
-        CRGB::White, 
-        CRGB::White,
-        CRGB::Goldenrod, 
-
-        CRGB::Goldenrod, 
-        CRGB::Orange, 
-        CRGB::Orange,
-        CRGB::Goldenrod  
-  );
-
-
-CRGBPalette16 lateDepoPal = CRGBPalette16 (
-  CRGB::Orange,
-  CRGB::DarkOrange,
-  CRGB::DarkOrange, 
-
-  CRGB::DarkOrange,
-  CRGB::Black, 
-  CRGB::DarkOrange, 
-
-  CRGB::Orange,
-  CRGB::DarkOrange, 
-  CRGB::DarkOrange,
-  
-  CRGB::DarkOrange,
-  CRGB::Black, 
-  CRGB::DarkOrange,  
-
-  CRGB::Orange,
-  CRGB::DarkOrange, 
-  CRGB::DarkOrange,
-
-  CRGB::Black
-  );
-  
-
-
-
-CRGBPalette16 repolarPal = CRGBPalette16 (
-        CRGB::Orange, 
-        CRGB::Goldenrod, 
-        CRGB::Goldenrod,
-        CRGB::Goldenrod, 
-
-        CRGB::Goldenrod, 
-        CRGB::White, 
-        CRGB::White, 
-        CRGB::White, 
-
-        CRGB::Orange,
-        CRGB::Goldenrod, 
-        CRGB::Goldenrod,
-        CRGB::Goldenrod, 
-
-        CRGB::Goldenrod, 
-        CRGB::White, 
-        CRGB::White,
-        CRGB::White  
-  );
-
-  
- 
 
 void setup() {
   
@@ -246,11 +183,11 @@ void setup() {
 
 }
 
-SimplePatternList gPatterns = { restingState,earlyDepolarization,lateDepolarization, repolarization, hyperpolarization};
 
 void loop() {
   recieveChar(); 
   commandLight();
+  
 }
 void serialFlush(){
   while(Serial.available() > 0) {
@@ -371,67 +308,53 @@ void commandLight(){
 void restingStateStrip(){
   for(int i = 0; i < NUM_LEDS; i++ )
    {
-    
+  
    leds[i] = CRGB::White;
-   leds[i].fadeLightBy(Restbrightness);
+   
+   
   }
-  FastLED.show();
-  Restbrightness = Restbrightness + RestfadeAmount;
-  // reverse the direction of the fading at the ends of the fade: 
-  if(Restbrightness == 0 || Restbrightness == 255)
-  {
-    RestfadeAmount = -RestfadeAmount ; 
-  }    
-  delay(9);
+   
+    FastLED.show();
+ 
   }
 
 void earlyDepoStrip(){
-   fill_palette(leds, NUM_LEDS, paletteIndex, 255 / NUM_LEDS, earlyDepoPal, 255, LINEARBLEND);
-  //leds, nLEDS, startIndex, indexDelta, palette, brightness, blendType
-  FastLED.show();
-
-  EVERY_N_MILLISECONDS(15){
-    paletteIndex++;
-    }
+  for(int i = 0; i < NUM_LEDS; i++ )
+   {
   
+   leds[i] = CRGB::Goldenrod;
+   
+   
+  }
+   
   }
 
 void lateDepoStrip(){
-   fill_palette(leds, NUM_LEDS, paletteIndex, 255 / NUM_LEDS, lateDepoPal, 255, LINEARBLEND);
-  //leds, nLEDS, startIndex, indexDelta, palette, brightness, blendType
-  FastLED.show();
-
-  EVERY_N_MILLISECONDS(10){
-    paletteIndex++;
-    }
+   for(int i = 0; i < NUM_LEDS; i++ )
+   {
+  
+   leds[i] = CRGB::Orange;
+   
+   
+  }
   }
 
 void repolarizationStrip(){
- fill_palette(leds, NUM_LEDS, paletteIndex, 255 / NUM_LEDS, repolarPal, 255, LINEARBLEND);
-  //leds, nLEDS, startIndex, indexDelta, palette, brightness, blendType
-  FastLED.show();
-
-  EVERY_N_MILLISECONDS(25){
-    paletteIndex++;
-    } 
+  for(int i = 0; i < NUM_LEDS; i++ )
+   {
+  
+   leds[i] = CRGB::Gold;
+   
+   
+  }
   }
 
 void hyperpolarizationStrip(){
    for(int i = 0; i < NUM_LEDS; i++ )
    {
-    
-   leds[i].setRGB(255,255,255);
-   leds[i].fadeLightBy(Hypbrightness);
+   leds[i] = CRGB::SkyBlue;
+   
   }
-  FastLED.show();
-  Hypbrightness = Hypbrightness + HypfadeAmount;
-  // reverse the direction of the fading at the ends of the fade: 
-  if(Hypbrightness == 0 || Hypbrightness == 100)
-  {
-    HypfadeAmount = -HypfadeAmount ; 
-  }    
-  delay(4);
-  
   }
 
 
@@ -441,64 +364,60 @@ void Particles(CRGBPalette16 pal1,CRGBPalette16 pal2){
     //Switch on an LED at random, choosing a random color from the palette
     int rando1 = random8(0, NUM_LEDS - 50);
     partLeds[rando1] = ColorFromPalette(pal1, random8(), 255, LINEARBLEND);
-    leds[rando1] = ColorFromPalette(pal1, random8(), 255, LINEARBLEND);
+    
+    
+     
+    
   }
 
   EVERY_N_MILLISECONDS(20) {
     int rando2 = random8(51, NUM_LEDS); 
     //Switch on an LED at random, choosing a random color from the palette
     partLeds[rando2] = ColorFromPalette(pal2, random8(), 255, LINEARBLEND);
-    leds[rando2] = ColorFromPalette(pal2, random8(), 255, LINEARBLEND);
+   
+    
   }
 
   // Fade all LEDs down by 1 in brightness each time this is called
-  fadeToBlackBy(partLeds, NUM_LEDS, 1);
+  fadeToBlackBy(partLeds, 50, 1);
 
   FastLED.show();
   }
 
   void restingState(){
     restingStateStrip(); 
-    Particles(restingPurpPal,restingGreenPal);
+    Particles(restingPurplePal,restingGreenPal);
     }
 
   void earlyDepolarization(){
     earlyDepoStrip();
-    Particles(depoPurpPal,restingGreenPal);
+    Particles(earlyDepoGreen,restingPurplePal);
     }
   void lateDepolarization(){
     lateDepoStrip(); 
-    Particles(depoPurpPal,restingGreenPal);
+    Particles(depoPurpPal,depoPurpPal);
     }
 
-    
    void repolarization(){
     repolarizationStrip(); 
     Particles(repolPurplePal,repolGreenPal);
     }
 
-    
     void hyperpolarization(){
      hyperpolarizationStrip(); 
-     Particles(restingPurpPal,hypGreenPal);
+     Particles(hypGreenPal,hypGreenPal);
       }
+   
 
-     void nextPattern()
-{
-  // add one to the current pattern number, and wrap around at the end
-  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
-}
+    
 
     void lightShowStrip(int n){
-      int period = n * 1000;
-     for( uint32_t tStart = millis();  (millis()-tStart) < period;  ){
-       gPatterns[ gCurrentPatternNumber]();
-       FastLED.delay(1000/FRAMES_PER_SECOND);
-       EVERY_N_SECONDS( 10 ) { nextPattern(); } 
-     }
-     gCurrentPatternNumber = 0; 
+      restingForN(10); 
+      earlyDepoForN(10);
+      lateDepoForN(10); 
+      repoForN(10);
+      hypForN(10);
       }
-
       void offStrip(){
             fill_solid(leds, NUM_LEDS, CRGB::Black);
             fill_solid(partLeds, NUM_LEDS, CRGB::Black);
@@ -507,32 +426,32 @@ void Particles(CRGBPalette16 pal1,CRGBPalette16 pal2){
 
   
   void restingForN(int n){
-    int period = n * 1000;
+    uint32_t period = n * 1000L;
      for( uint32_t tStart = millis();  (millis()-tStart) < period;  ){
         restingState();
       }
     }
   void earlyDepoForN(int n){
-    int period = n * 1000;
+    uint32_t period = n * 1000L;
      for( uint32_t tStart = millis();  (millis()-tStart) < period;  ){
         earlyDepolarization();
       }
     }
     void lateDepoForN(int n){
-    int period = n * 1000;
+    uint32_t period = n * 1000L;
      for( uint32_t tStart = millis();  (millis()-tStart) < period;  ){
         lateDepolarization();
       }
     }
 
      void repoForN(int n){
-    int period = n * 1000;
+    uint32_t period = n * 1000L;
      for( uint32_t tStart = millis();  (millis()-tStart) < period;  ){
         repolarization();
       }
     }
      void hypForN(int n){
-    int period = n * 1000;
+    uint32_t period = n * 1000L;
      for( uint32_t tStart = millis();  (millis()-tStart) < period;  ){
         hyperpolarization();
       }
